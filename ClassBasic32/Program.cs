4e130c13e6e40f4147348445f4dbca230f5293ec
Ye-jin Lee <s2108@e-mirim.hs.kr>
2023-05-09 11:06:47 +0900

    클래스 이름이 겹치는 경우

--- ClassBasic32/Program.cs
+++ ClassBasic32/Program.cs
@@ -8,8 +8,29 @@ namespace ClassBasic32
 {
     internal class Program
     {
+        private class Math
+        {
+            /*
+            static public int Abs(int a)
+            {
+                return 0;
+            }*/
+        }
         static void Main(string[] args)
         {
+            Math math = new Math();
+            //Console.WriteLine(Math.Abs(-10));
+
+            ThirdClass abc;
+            ABCClass abc2;
+            InnerClass inn;
+            Console.WriteLine("Hello World!");
         }
+        private class InnerClass
+        {
+        }
+    }
+    internal class ABCClass
+    {
     }
 }
